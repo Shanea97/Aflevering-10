606d4ead41fa6434f08ca5d7d8bda0fde93bfc12
Bertram P <bertramherlov@gmail.com>
2018-12-23 12:42:57 +0100

    WolfHunger.fsx er opdateret
    WolfHunger har nu en "funktion" hvor jeg har leget med Elgenes reproduktion. Her får den smidt en elg en, og den opdatere tick for listen hele tiden for alle elgene, derfor vokser den eksponentielt xD

--- Assaignment/WolfHunger.fsx
+++ Assaignment/WolfHunger.fsx
@@ -61,18 +61,56 @@ type wolf (repLen : int, hungLen : int) = // hungLen is its hunger counter
     else
         None
 
+
+let AplhaMoose = moose(5)
+let mutable MooseList = [AplhaMoose]
+for i = 0 to 25 do 
+  for j = 0 to MooseList.Length-1 do 
+    MooseList.[j].tick()
+    // printfn "Moose %i's RepLen: %A" j MooseList.[j].reproduction
+    if MooseList.[j].reproduction = 1 then 
+      MooseList <- moose(5) :: MooseList
+  printfn "MooseList: %A" MooseList
+
+ 
+ 
+ 
+ 
+ 
+  // AplhaMoose.tick()
+  // printfn "AlphaMoose RepLen: %A" AplhaMoose.reproduction
+  // if AplhaMoose.reproduction = 1 then 
+  //   MooseList <- moose(5) :: MooseList
+  //   printfn "MooseList: %A" MooseList
+  // else 
+  //   for i = 1 to MooseList.Length-1 do 
+  //     MooseList.[i].tick ()
+  //     printfn "Moose %i's RepLen %i" i MooseList.[i].reproduction
+  //     if AplhaMoose.reproduction = 1 then 
+  //     MooseList <- moose(5) :: MooseList
+  //     printfn "MooseList: %A" MooseList
+      
+
+
+
+
 let AplhaWolf = wolf(5,7)
+
+
 
 let BetaWolf = wolf(5,2)
 
 let CharlieWolf = wolf(5,8)
 
 let DeltaWolf = wolf(5,2)
-let mutable WolfList = [|AplhaWolf;BetaWolf;CharlieWolf;DeltaWolf|]
-for j = 0 to WolfList.Length-1 do 
-    WolfList <- WolfList |> Array.filter (fun x -> x.hunger > 0)
-    printfn "WolfList: %A" WolfList
-    for i = 0 to WolfList.Length-1 do 
-        WolfList.[i].tick()
-        printfn "Wolf %A's Hunger: %A" i WolfList.[i].hunger
+
+let EchoWolf = wolf(5,6)
+// let mutable WolfList = [|AplhaWolf;BetaWolf;CharlieWolf;DeltaWolf|]
+// for j = 0 to WolfList.Length-1 do 
+//     WolfList <- WolfList |> Array.filter (fun x -> x.hunger > 0)
+//     printfn "WolfList: %A" WolfList
+//     for i = 0 to WolfList.Length-1 do 
+//         WolfList.[i].tick()
+//         printfn "Wolf %A's Hunger: %A" i WolfList.[i].hunger
+//         printfn "Wolf %A's RepLen: %A" i WolfList.[i].reproduction
 
