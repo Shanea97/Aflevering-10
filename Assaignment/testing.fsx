285980d714a63dbe6b0883c7fb91e57633727072
MSAC98 <sammy.chauhan27@gmail.com>
2019-01-02 00:01:09 +0100

    Update Assaignment/testing.fsx
    White-box test for:
    Ulvene og elgene reproducere
    Ulvene dør af sult
    Ulvene æder elgene

--- Assaignment/testing.fsx
+++ Assaignment/testing.fsx
@@ -47,26 +47,6 @@ let a1 = moose (3)
 a1.resetReproduction()
 printfn "With the following input for moose: \nrepLen: %i" 3
 printfn "Does repLen for the moose reset to the initial repLen with resetReproduction? %b\n" (a1.reproduction=3) //expected output true 
-(*
-printfn "White-Box Test for reproduction of new moose"
-let a2 = moose (0)
-printfn "With the following input for moose: \nrepLen: %i" 0
-printfn "Does repLen for the moose reset to the initial repLen with resetReproduction? %b\n" (a2 =  ) //expected output true 
-*)
-
-
-printfn "Test af Moose, repLen 4"
-let testMoose = moose (4)
-printfn "reproduction: %A" (testMoose.reproduction)
-testMoose.updateReproduction ()
-printfn "updateReproduction: %A" (testMoose.reproduction)
-testMoose.updateReproduction ()
-printfn "updateReproduction: %A" (testMoose.reproduction)
-testMoose.updateReproduction ()
-printfn "updateReproduction: %A" (testMoose.reproduction)
-testMoose.resetReproduction ()
-printfn "resetReproduction: %A" (testMoose.reproduction)
-
 
 /// A wolf is an animal with a hunger counter
 type wolf (repLen : int, hungLen : int) = // hungLen is its hunger counter
@@ -100,34 +80,6 @@ b1.resetReproduction()
 printfn "With the following input for wolf: \nrepLen: %i \n hungLen: %i" 7 4 
 printfn "Does repLen for the wolf reset to the initial repLen with resetReproduction? %b\n" (b1.reproduction=7) //expected output true 
 
-(*
-printfn "White-Box Test for reproduction of new wolf"
-let b2 = wolf (0,6)
-b2.reproduction
-printfn "With the following input for wolf: \nrepLen: %i \n hungLen: %i" 0 6 
-printfn "Does repLen for the wolf reset to the initial repLen with resetReproduction? %b\n" () //expected output true 
-*)
-
-let newWolf = wolf (5,6)
-printfn "Hunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.updateHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
-newWolf.resetHunger()
-printfn "UpdateHunger: %A" (newWolf.hunger)
 
 /// A board is a chess-like board implicitly representedy by its width and coordinates of the animals.
 type board =
@@ -173,16 +125,13 @@ type environment (boardWidth : int, NMooses : int, mooseRepLen : int, NWolves : 
   member this.board = _board
   member this.tick () =
 
-    _board.wolves <- _board.wolves |> List.filter (fun x -> x.hunger > 0) // Fjerner ulve fra listen som har sult 0
-
-    _board.moose <- _board.moose |> List.filter (fun x -> x.position = None) // Fjerner elge fra listen som har position None
-
+    _board.wolves <- _board.wolves |> List.filter (fun x -> x.hunger >= 0) // Fjerner ulve fra listen som har sult 0
 
     for i = 0 to _board.moose.Length - 1 do
 
       match _board.moose.[i].tick () with
         | Some (moose) ->
-          if not _board.moose.[i].position.IsSome then
+          if _board.moose.[i].position = None then
             moose.position <- Some (anyEmptyField _board)
             _board.moose <- moose  :: _board.moose
         | None ->
@@ -191,7 +140,7 @@ type environment (boardWidth : int, NMooses : int, mooseRepLen : int, NWolves : 
     for i = 0 to _board.wolves.Length - 1 do
       match _board.wolves.[i].tick () with
         | Some (wolf) ->
-          if not _board.wolves.[i].position.IsSome then
+          if _board.wolves.[i].position = None then
             wolf.position <- Some (anyEmptyField _board)
             _board.wolves <- wolf :: _board.wolves
         | None ->
@@ -218,15 +167,13 @@ type environment (boardWidth : int, NMooses : int, mooseRepLen : int, NWolves : 
           _board.wolves.[j].resetHunger ()
           _board.moose.[i].position <- None
         //ulv ovenfor
-        elif (fst _board.wolves.[j].position.Value, snd _board.wolves.[j].position.Value - 1) = _board.moose.[j].position.Value then
+        else if (fst _board.wolves.[j].position.Value, snd _board.wolves.[j].position.Value - 1) = _board.moose.[j].position.Value then
           _board.wolves.[j].position <- Some (_board.moose.[i].position.Value)
           _board.wolves.[j].resetHunger ()
           _board.moose.[i].position <- None
-
-
-
-
-
+      
+        _board.moose <- _board.moose |> List.filter (fun x -> x.position.IsSome) 
+      
 
   override this.ToString () =
     let arr = draw _board
@@ -241,3 +188,46 @@ type environment (boardWidth : int, NMooses : int, mooseRepLen : int, NWolves : 
       ret <- ret + "\n"
     ret
 
+
+printfn "White-Box Test for wolves eating mooses"
+let x = environment(10, 30, 11, 10, 11, 11, true)
+printfn "With the following input: \nboardWidth: %i \nmoose: %i \nmooseRepLen: %i \nwolves: %i \nwolvesRepLen: %i \nwolvesHungLen: %i" 10 30 11 10 11 11 
+for i = 1 to 10 do
+  x.tick()
+  printfn "%i. tick: Does any of the mooses get eaten by wolves? %b" i (x.board.moose.Length < 30) //expected outcome true
+
+printfn "White-Box Test for wolves reproduction"
+let x1 = environment(10, 0, 0, 10, 4, 11, true)
+printfn "With the following input: \nboardWidth: %i \nmoose: %i \nmooseRepLen: %i \nwolves: %i \nwolvesRepLen: %i \nwolvesHungLen: %i" 10 0 0 10 4 11 
+for i = 1 to 15 do
+  x1.tick()
+  printfn "%i. tick: Does the wolves reproduce? Is there still 10 wolves at maximum? %b" i (x1.board.wolves.Length > 10) //expected outcome true
+
+printfn "White-Box Test for mooses reproduction"
+let x2 = environment(10, 30, 5, 0, 0, 0, true)
+printfn "With the following input: \nboardWidth: %i \nmoose: %i \nmooseRepLen: %i \nwolves: %i \nwolvesRepLen: %i \nwolvesHungLen: %i" 10 0 30 0 0 0 
+for i = 1 to 15 do
+  x2.tick()
+  printfn "%i. tick: Does the mooses reproduce? %b" i (x2.board.moose.Length > 30) //expected outcome true
+
+printfn "White-Box Test for wolves death from hunger"
+let x3 = environment(10, 0, 0, 10, 10, 2, true)
+printfn "With the following input: \nboardWidth: %i \nmoose: %i \nmooseRepLen: %i \nwolves: %i \nwolvesRepLen: %i \nwolvesHungLen: %i" 10 0 0 10 10 2
+for i = 1 to 5 do
+  x3.tick()
+  printfn "%i. tick: Does any of the wolves die from hunger? %b" i (x3.board.wolves.Length < 10) //expected outcome true
+
+
+(*
+let NewEnvironment = environment(10, 4, 2, 5, 4, 8, true)
+
+for i = 0 to 5 do
+  NewEnvironment.tick()
+  printfn "%s" (NewEnvironment.ToString())
+  for j = 0 to NewEnvironment.board.moose.Length-1 do   
+    printfn "Moose %i's RepLen: %A\nMoose %i's position: %A" j NewEnvironment.board.moose.[j].reproduction  j NewEnvironment.board.moose.[j].position
+  for x = 0 to NewEnvironment.board.wolves.Length-1 do 
+    printfn "Wolf %i's RepLen: %A\nWolf %i's position: %A" x NewEnvironment.board.wolves.[x].reproduction x NewEnvironment.board.wolves.[x].position
+*)
+
+//
